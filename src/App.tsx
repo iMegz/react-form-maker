196e2e996220c8b3f8f9a907eb40bb23b8602f88
Ahmed Magdi <ammhaa2012@gmail.com>
2023-10-31 17:29:46 +0200

    Add loading page

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,31 @@ import FormPage from "./pages/FormPage";
 import DashboardLayout from "./layouts/DashboardLayout";
 import { useAuth0 } from "@auth0/auth0-react";
 import HomePage from "./pages/HomePage";
+import LoadingPage from "./pages/LoadingPage";
 
 function App() {
-  const { isAuthenticated } = useAuth0();
-  const Home = isAuthenticated ? DashboardLayout : HomePage;
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  function setRoutes() {
+    if (isLoading) return <Route path="/" element={<LoadingPage />} />;
+    if (!isAuthenticated) return <Route path="/" element={<HomePage />} />;
+    return (
+      <Route path="/" element={<DashboardLayout />}>
+        <Route index element={<Dashboardpage />} />
+        <Route path="forms">
+          <Route index element={<FormsPage />} />
+          <Route path="edit/:id?" element={<EditFormPage />} />
+          <Route path="preview/:id?" element={<FormPage preview />} />
+        </Route>
+        <Route path="settings" element={<SettingsPage />} />
+      </Route>
+    );
+  }
 
   return (
     <div className="flex bg-slate-100 max-h-max">
       <Routes>
-        <Route path="/" element={<Home />}>
-          <Route index element={<Dashboardpage />} />
-          <Route path="forms">
-            <Route index element={<FormsPage />} />
-            <Route path="edit/:id?" element={<EditFormPage />} />
-            <Route path="preview/:id?" element={<FormPage preview />} />
-          </Route>
-          <Route path="settings" element={<SettingsPage />} />
-        </Route>
+        {setRoutes()}
         <Route path="form/:id" element={<FormPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
