e2fd300b35ba516d29a561f4f0bc05f7669f59e6
Ahmed Magdi <ammhaa2012@gmail.com>
2023-10-31 14:29:27 +0200

    Add about section

--- src/components/HomeNavbar.tsx
+++ src/components/HomeNavbar.tsx
@@ -6,6 +6,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const links = [
   { label: "About", hash: "about" },
   { label: "Features", hash: "features" },
+  { label: "Pricing", hash: "pricing" },
   { label: "Contact us", hash: "contact" },
 ];
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -32,6 +32,38 @@ const HomePage = () => {
         </div>
         <img src={hero} alt="Hero image" className="hidden w-1/2 lg:block" />
       </div>
+
+      {/* About */}
+      <section className="bg-white " id="about">
+        <div className="container text-center">
+          <h2 className="text-5xl">
+            What is
+            <span className="font-semibold text-primary"> Forms Chief </span>?
+          </h2>
+          <hr className="w-32 border-primary" />
+          <div className="flex flex-col w-full gap-3 text-lg text-gray-500 lg:w-3/4">
+            <p>
+              Forms Chief is your all-in-one platform for creating, managing,
+              and analyzing forms and surveys. We're here to simplify your data
+              collection process and empower you with insightful analysis.
+            </p>
+            <p>
+              At Forms Chief, we believe that every piece of data has a story to
+              tell, and we're here to help you uncover those stories.
+            </p>
+            <p>
+              Whether you're a seasoned professional or new to the world of
+              data, our platform provides the tools and insights you need to
+              make data work for you. With user-friendly form creation, powerful
+              analysis, and customizable survey options, Forms Chief offers a
+              comprehensive solution for individuals and businesses alike. We're
+              more than just a tool; we're your partner in data excellence. Join
+              us on this data-driven journey, and discover the possibilities
+              that Forms Chief brings to your fingertips
+            </p>
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
