26a5f462ff1a973c2b5510036c27f325d151eff3
Ahmed Magdi <ammhaa2012@gmail.com>
2023-11-05 00:14:09 +0200

    Add retrieve all forms, and delete form

--- src/pages/FormsPage.tsx
+++ src/pages/FormsPage.tsx
@@ -5,23 +5,52 @@ import {
   UnorderedListOutlined,
 } from "@ant-design/icons";
 import { Link } from "react-router-dom";
-import { ReactNode, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import Modal from "../components/Modal";
 import { createPortal } from "react-dom";
-import { forms } from "../lib/data";
+import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
+import Spinner from "../components/Spinner";
 
 const FormsPage = () => {
   const [modal, setModal] = useState<ReactNode | null>(null);
+  const [forms, setForms] = useState<Form[] | null>(null);
+  const { getAccessTokenSilently } = useAuth0();
 
-  function deleteForm(id: string) {
-    // Call api to delete form
-    console.log(id);
-    const index = forms.findIndex((v) => v.id == id);
-    forms.splice(index, 1);
+  // Get forms from backend
+  useEffect(() => {
+    async function fetchData() {
+      const path = `${import.meta.env.VITE_API}/forms/get/all`;
+      const token = await getAccessTokenSilently();
+      const authorization = `Bearer ${token}`;
+      const res = await axios.get(path, { headers: { authorization } });
+      return res;
+    }
+
+    fetchData()
+      .then((res) => setForms(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  // Delete form
+  async function deleteForm(id: string, cb: Function) {
+    // Delete it from frontend
+    const index = forms!.findIndex((v) => v.id == id);
+    forms!.splice(index, 1);
     setModal(null);
+
+    // Delete it from backend
+    const path = `${import.meta.env.VITE_API}/forms/del/${id}`;
+    const token = await getAccessTokenSilently();
+
+    const authorization = `Bearer ${token}`;
+    await axios.delete(path, { headers: { authorization } });
+    cb();
   }
 
+  // Delete form modal component
   function DeleteModal({ title, id }: { title: string; id: string }) {
+    const [isLoading, setIsLoading] = useState(false);
     return (
       <Modal hideOneClickOutside hide={() => setModal(null)}>
         <h4>
@@ -30,7 +59,14 @@ const FormsPage = () => {
           irreversible ?
         </h4>
         <div className="flex justify-end gap-2 mt-3">
-          <button onClick={() => deleteForm(id)} className="btn-danger">
+          <button
+            disabled={isLoading}
+            onClick={() => {
+              setIsLoading(true);
+              deleteForm(id, () => setIsLoading(false));
+            }}
+            className="btn-danger"
+          >
             Delete
           </button>
           <button onClick={() => setModal(false)} className="btn">
@@ -39,6 +75,74 @@ const FormsPage = () => {
         </div>
       </Modal>
     );
+  }
+
+  // Render forms table
+  function renderForms() {
+    if (!forms) {
+      return (
+        <tr>
+          <td colSpan={3}>
+            <Spinner className="flex justify-center" />
+          </td>
+        </tr>
+      );
+    }
+
+    if (!forms.length) {
+      return (
+        <tr>
+          <td colSpan={3} className="">
+            <span className="block text-lg text-center text-slate-400">
+              You didn't create any forms yet
+            </span>
+          </td>
+        </tr>
+      );
+    }
+
+    return forms.map(({ id, title, description }) => (
+      <tr key={id}>
+        <td className="text-xs md:text-base">
+          <Link to={"/forms/edit/" + id}>{title}</Link>
+        </td>
+
+        <td className="hidden lg:table-cell ">
+          <p className="line-clamp-2">{description}</p>
+        </td>
+
+        <td className="flex gap-0 md:gap-2">
+          <Link to={"/forms/preview/" + id}>
+            <button className="btn-text-primary">
+              <EyeFilled />
+              <span className="hidden md:inline">Preview</span>
+            </button>
+          </Link>
+
+          <Link to={"/forms/responses/" + id}>
+            <button className="btn-text">
+              <UnorderedListOutlined />
+              <span className="hidden md:inline">Responses</span>
+            </button>
+          </Link>
+
+          <Link to={"/forms/edit/" + id}>
+            <button className="btn-text-warning">
+              <EditFilled />
+              <span className="hidden md:inline">Edit</span>
+            </button>
+          </Link>
+
+          <button
+            className="btn-text-danger"
+            onClick={() => setModal(<DeleteModal id={id} title={title} />)}
+          >
+            <DeleteFilled />
+            <span className="hidden md:inline">Delete</span>
+          </button>
+        </td>
+      </tr>
+    ));
   }
 
   return (
@@ -60,52 +164,7 @@ const FormsPage = () => {
               <th scope="col">Actions</th>
             </tr>
           </thead>
-          <tbody>
-            {forms.map(({ id, title, description }) => (
-              <tr key={id}>
-                <td className="text-xs md:text-base">
-                  <Link to={"/forms/edit/" + id}>{title}</Link>
-                </td>
-
-                <td className="hidden lg:table-cell ">
-                  <p className="line-clamp-2">{description}</p>
-                </td>
-
-                <td className="flex gap-0 md:gap-2">
-                  <Link to={"/forms/preview/" + id}>
-                    <button className="btn-text-primary">
-                      <EyeFilled />
-                      <span className="hidden md:inline">Preview</span>
-                    </button>
-                  </Link>
-
-                  <Link to={"/forms/responses/" + id}>
-                    <button className="btn-text">
-                      <UnorderedListOutlined />
-                      <span className="hidden md:inline">Responses</span>
-                    </button>
-                  </Link>
-
-                  <Link to={"/forms/edit/" + id}>
-                    <button className="btn-text-warning">
-                      <EditFilled />
-                      <span className="hidden md:inline">Edit</span>
-                    </button>
-                  </Link>
-
-                  <button
-                    className="btn-text-danger"
-                    onClick={() =>
-                      setModal(<DeleteModal id={id} title={title} />)
-                    }
-                  >
-                    <DeleteFilled />
-                    <span className="hidden md:inline">Delete</span>
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
+          <tbody>{renderForms()}</tbody>
         </table>
       </div>
 
